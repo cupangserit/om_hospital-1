8e338b53cab8f74dd08638d7d47980909e537471
Ade Setiawan <adesbsp@gmail.com>
2020-07-14 11:59:52 +0700

    record name
    add the record name and default value name
    
    Co-Authored-By: Ade Setiawan <adesbsp@gmail.com>

--- models/patient.py
+++ models/patient.py
@@ -4,9 +4,12 @@ class HospitalPatient(models.Model):
     # define name of table
     _name = 'hospital.patient'
     _description = 'Patient Record'
+    _rec_name = 'patient_name'
 
     # define field of patient
     patient_name = fields.Char('Name', required=True)
     patient_age = fields.Integer('Age')
     notes = fields.Text('Notes')
     image = fields.Binary('Image')
+    name = fields.Char('Test')
+
