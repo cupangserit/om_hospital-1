6f4283620fa0dd883651a3ce6f61883d14019e94
Ade Setiawan <adesbsp@gmail.com>
2020-07-14 10:55:10 +0700

    Add Tree view and Form View
    Create Form view and tree view in odoo 12 for patient record
    
    Co-Authored-By: Ade Setiawan <adesbsp@gmail.com>

--- models/patient.py
+++ models/patient.py
@@ -1,9 +1,11 @@
 from odoo import fields, models, api
 
 class HospitalPatient(models.Model):
+    # define name of table
     _name = 'hospital.patient'
     _description = 'Patient Record'
 
+    # define field of patient
     patient_name = fields.Char('Name', required=True)
     patient_age = fields.Integer('Age')
     notes = fields.Text('Notes')
