77d121f1b65a065ca81700ddd8891ee38190974b
Ade Setiawan <adesbsp@gmail.com>
2020-07-14 09:17:59 +0700

    step 1 building odoo module

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'Hospital Management',
+    'version': '12.0.1.0.0',
+    'summary': 'Module for Manage Hospital',
+    'description': """ 
+    Feature Module \n
+        - Manage Hospital 
+        - Manage Patient 
+        - Manage doktor 
+        - Manage Lab 
+        - Manage Pharmacy 
+        - Manage Appointment
+    """,
+    'category': 'Extra Tools',
+    'author': 'LobotIjo',
+    'maintainer':'AdesDev',
+    'website': 'digitalfarmer.github.io',
+    'license': 'AGPL-3',
+    'depends': [
+        'base'
+    ],
+    'data': [
+
+    ],
+    'demo': [''],
+    'installable': True,
+    'application': True,
+    'sequence': '5',
+    'auto_install': False,
+}
