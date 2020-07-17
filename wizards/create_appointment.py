fe8f4a801e7decee2e2b63f0b10ea212b8f7d734
Ade Setiawan <adesbsp@gmail.com>
2020-07-17 10:10:55 +0700

    Create POST function wizard form
    save wizard from action button
    
    Co-Authored-By: Ade Setiawan <adesbsp@gmail.com>

--- wizards/create_appointment.py
+++ wizards/create_appointment.py
@@ -6,3 +6,11 @@ class CreateAppointment(models.TransientModel):
 
     patient_id= fields.Many2one('hospital.patient', 'Patient')
     appointment_date= fields.Date('Date')
+
+    def create_appointment(self):
+        vals= {
+            'patient_id': self.patient_id.id,
+            'appointment_date': self.appointment_date,
+            'notes': 'Create From the Wizard/Code'
+        }
+        self.env['hospital.appointment'].create(vals)
