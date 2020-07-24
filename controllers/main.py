501a49337ff4acd4b889b1a2ee97a3c29864c137
Ades <ades@binasanprima.com>
2020-07-24 15:09:34 +0700

    mobile api part2
    update record and logout session

--- controllers/main.py
+++ controllers/main.py
@@ -11,11 +11,23 @@ class Hospital(http.Controller):
             'patients':patients
         })
 
+    # api to update patient data
+    @http.route('/update_patient', type="json", auth="user")
+    def update_patient(self,**rec):
+        if request.jsonrequest:
+            if rec['id']:
+                patient = request.env['hospital.patient'].sudo().search([('id','=',rec['id'])])
+                if patient:
+                    patient.sudo().write(rec)
+                args = {'success':True, 'message':'Success'}
+        return args
+
+
     # building api odoo to get patient
     @http.route('/create_patient',type='json', auth='user')
     def create_patient(self,**rec):
         if request.jsonrequest:
-            print('rec',rec)
+            # print('rec',rec)
             if rec ['name']:
                 vals ={
                     'patient_name': rec['name'],
@@ -24,7 +36,7 @@ class Hospital(http.Controller):
                 new_patient = request.env['hospital.patient'].sudo().create(vals)
                 args ={'success':True, 'message':'Success', 'ID':new_patient.id}
             return args
-
+    # api to get patient record
     @http.route('/get_patients', type='json', auth='user')
     def get_patients(self):
         patients_rec = request.env['hospital.patient'].search([])
