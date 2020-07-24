e94d9d85ed7593a8f4ef30be8d7eaf55f667115e
Ades <ades@binasanprima.com>
2020-07-24 11:26:00 +0700

    odoo developing API mobile version
    Login, fetching, create field

--- controllers/main.py
+++ controllers/main.py
@@ -11,4 +11,31 @@ class Hospital(http.Controller):
             'patients':patients
         })
 
-    #@http.route('/hospital/doctor'):
+    # building api odoo to get patient
+    @http.route('/create_patient',type='json', auth='user')
+    def create_patient(self,**rec):
+        if request.jsonrequest:
+            print('rec',rec)
+            if rec ['name']:
+                vals ={
+                    'patient_name': rec['name'],
+                    'email_id': rec['email_id']
+                }
+                new_patient = request.env['hospital.patient'].sudo().create(vals)
+                args ={'success':True, 'message':'Success', 'ID':new_patient.id}
+            return args
+
+    @http.route('/get_patients', type='json', auth='user')
+    def get_patients(self):
+        patients_rec = request.env['hospital.patient'].search([])
+        patients=[]
+        for rec in patients_rec:
+            vals ={
+                "id":rec.id,
+                "name": rec.patient_name,
+                "age": rec.patient_age,
+                "patient_id": rec.name_seq,
+            }
+            patients.append(vals)
+        data ={'status':200, 'response':patients, 'message': 'Success'}
+        return data
